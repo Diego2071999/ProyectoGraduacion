3dcb199e9d6ee29b1bf7c1808b549730267f3daa
Diego2071999 <101607453+Diego2071999@users.noreply.github.com>
2024-10-23 06:47:17 -0600

    Delete Version 4/src directory

--- Version 4/src/rutas/rutafact.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import { methods as factmethods} from "../controler/factcontroler.js";
-
-const factruta = Router();
-
-factruta.get("/fact", factmethods.getAllFacWithRelations);
-factruta.post("/fact", factmethods.createFactura);
-
-export default  factruta;
