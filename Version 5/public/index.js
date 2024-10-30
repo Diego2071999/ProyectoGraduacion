0b937eed3b8eb2cd290e60c8570f18961d7e8de3
Diego2071999 <101607453+Diego2071999@users.noreply.github.com>
2024-10-29 18:29:35 -0600

    Delete Version 5/public directory

--- Version 5/public/index.js
+++
@@ -1,8 +0,0 @@
-fetch('http://localhost:4000')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); // Aquí puedes manejar los datos recibidos del backend
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
