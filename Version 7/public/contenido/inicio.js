bb769941573a5659dc24ca14b0e5c6be71c47e52
Diego2071999 <101607453+Diego2071999@users.noreply.github.com>
2024-11-03 11:28:19 -0600

    Delete Version 7/public directory

--- Version 7/public/contenido/inicio.js
+++
@@ -1,9 +0,0 @@
-// Animación de parpadeo
-document.querySelectorAll('.menu-item').forEach(item => {
-    item.addEventListener('mouseenter', () => {
-        item.classList.add('blink');
-    });
-    item.addEventListener('mouseleave', () => {
-        item.classList.remove('blink');
-    });
-});
